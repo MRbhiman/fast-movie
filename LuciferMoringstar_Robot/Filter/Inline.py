0a1d9db70e15000ee179df193fe9e987e0b86f4d
FASTmovie <92666116+FASTmovie@users.noreply.github.com>
2021-10-21 22:09:44 +0530

    Update Inline.py

--- LuciferMoringstar_Robot/Filter/Inline.py
+++ LuciferMoringstar_Robot/Filter/Inline.py
@@ -42,7 +42,7 @@ async def answer(bot, query):
         f_caption=file.caption
         if CUSTOM_FILE_CAPTION:
             try:
-                f_caption=CUSTOM_FILE_CAPTION.format(file_name=title, file_size=size, file_caption=f_caption)
+                f_caption=CUSTOM_FILE_CAPTION.format(file_name=🖤title❤, file_size=❤size🖤, file_caption=❤f_caption🖤)
             except Exception as e:
                 print(e)
                 f_caption=f_caption
@@ -89,7 +89,7 @@ async def answer(bot, query):
 
 def get_reply_markup(query):
     buttons = [[
-        InlineKeyboardButton('Deploy Video', url=f'{TUTORIAL}')
+        InlineKeyboardButton('🖤movie_channel❤', url="https://t.me/@mrbhiman12"
         ],[
         InlineKeyboardButton('🔍 Search again 🔎', switch_inline_query_current_chat=query)
         ]]

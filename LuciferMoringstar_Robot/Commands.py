59fb575ff032efe37359495a647a771515ca5e28
FASTmovie <92666116+FASTmovie@users.noreply.github.com>
2021-10-21 22:36:53 +0530

    Update Commands.py

--- LuciferMoringstar_Robot/Commands.py
+++ LuciferMoringstar_Robot/Commands.py
@@ -332,7 +332,7 @@ async def bot_info(bot, message):
     buttons = [
         [
             
-            InlineKeyboardButton('Deploy Video', url=f'{TUTORIAL}')
+            InlineKeyboardButton('😍MOVIE CHANNEL😍', url="https://t.me/mrbhiman12"')
         ]
         ]
     await message.reply(text=f"{ABOUT}", reply_markup=InlineKeyboardMarkup(buttons), disable_web_page_preview=True)

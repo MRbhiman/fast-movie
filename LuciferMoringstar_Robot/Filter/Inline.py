afcef5df4faff317e581b17c08017d9311b49590
『ϻᴿ』☯Sʜᴀᴅօw <86451921+MRbhiman@users.noreply.github.com>
2021-10-22 09:26:23 +0530

    Update Inline.py

--- LuciferMoringstar_Robot/Filter/Inline.py
+++ LuciferMoringstar_Robot/Filter/Inline.py
@@ -42,7 +42,7 @@ async def answer(bot, query):
         f_caption=file.caption
         if CUSTOM_FILE_CAPTION:
             try:
-                f_caption=CUSTOM_FILE_CAPTION.format(file_name=🖤title❤, file_size=❤size🖤, file_caption=❤f_caption🖤)
+                f_caption=CUSTOM_FILE_CAPTION.format(file_name=title, file_size=size, file_caption=f_caption)
             except Exception as e:
                 print(e)
                 f_caption=f_caption
